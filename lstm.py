5d6bf1f6361e32ec8afe130045910ef706464067
jykim <jykim256@gmail.com>
2020-04-24 21:44:39 -0700

    checkpoint saves

--- lstm.py
+++ lstm.py
@@ -13,6 +13,7 @@ from math import sqrt
 from matplotlib import pyplot
 from numpy import array
 import csv
+from tqdm import tqdm
 
 # convert time series into supervised learning problem
 def series_to_supervised(data, n_in=1, n_out=1, dropnan=True):
@@ -187,7 +188,12 @@ def generate_result():
 	unique_fips = [x for x in unique_fips if str(x) != 'nan']
 	print("{} fips total".format(len(unique_fips)))
 	prediction = [["id","10","20","30","40","50","60","70","80","90"]]
-	for i, fips in enumerate(unique_fips):
+	for i in tqdm(range(len(unique_fips))):
+		if i % CHECKPOINT == CHECKPOINT-1:
+			with open("predictions_%d.csv" % i, "w+") as f:
+				csv_writer = csv.write(f, delimeter = ",")
+				csv_writer.writerows(prediction)
+		fips = unique_fips[i]
 		print("Fips #{}: {}".format(i+1, fips))
 		series = data[data["fips"] == fips].drop(["fips", "county", "state"], axis=1)
 
